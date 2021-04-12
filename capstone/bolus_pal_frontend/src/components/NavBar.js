131a3a8e056aa5745e6eda416d1a05a30324d18e
spwinter000 <32625655+spwinter000@users.noreply.github.com>
2021-04-12 14:46:59 -0400

    another addition

--- capstone/bolus_pal_frontend/src/components/NavBar.js
+++ capstone/bolus_pal_frontend/src/components/NavBar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 
 const NavBar = ({loggedIn, handleLogout}) => {
 
-    // if user is logged in, show a certain navbar, else show another with login and register
+    // if user is logged in, show a certain navbar, else show another with login and sign-up
     const loggedInNav = (
         <nav className="navbar navbar-expand-lg navbar-light bg-white" id="navbar">
                 <Link className="navbar-brand" to="/boluses"><img id="logo" src={require('../images/boluspal-logo.png').default}/></Link>
@@ -47,34 +47,6 @@ const NavBar = ({loggedIn, handleLogout}) => {
         </nav >
     )
     return <div>{loggedIn ? loggedInNav : loggedOutNav}</div>
-// return(
-//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//                 <Link className="navbar-brand" to="/">BolusPal</Link>
-//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//                 <span className="navbar-toggler-icon"></span>
-//             </button>
-//             <div className="collapse navbar-collapse" id="navbarNav">
-//                 <div className="navbar-nav">
-//                     {/* <ul className="navbar-nav"> */}
-//                     {/* <ul className="navbar-nav"> */}
-//                     <li className="nav-item">
-//                             <NavLink className="nav-link" to="/boluses">My Boluses</NavLink>
-//                     </li>
-//                     <li className="nav-item">
-//                             <NavLink className="nav-link" to="/profile">Profile</NavLink>
-//                     </li>
-//                     <li className="nav-item">
-//                             <NavLink className="nav-link" to="/login">Login</NavLink>
-//                     </li>
-//                     <li className="nav-item">
-//                             <NavLink className="nav-link" to="/register">Register</NavLink>
-//                     </li>
-//                     {/* </ul> */}
-//                 </div>
-//             </div>
-//         </nav >
-// )
-
 }
 
 NavBar.propTypes = {
